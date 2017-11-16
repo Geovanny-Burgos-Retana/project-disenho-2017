c3d211ac15ffb9f74a7aa875af8482861c3d21ec
Geovanny Burgos <geoburgosretana@gmail.com>
2017-11-16 01:10:22 -0600

    prueba

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'client')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/', index);
+app.use('/api', index);
 app.use('/api', listas);
 app.use('/api', productos);
 
